cbd655ca047de6038eb60f0154a73881c64a344e
rambolee200311 <43191484+rambolee200311@users.noreply.github.com>
2025-03-28 17:31:32 +0100

    20250328update
    20250328update settle

--- mymodules/panexLogi/models/settle_clearance.py
+++ mymodules/panexLogi/models/settle_clearance.py
@@ -1,5 +1,8 @@
 from odoo import _, models, fields, api, exceptions
 from odoo.exceptions import UserError
+import xlsxwriter
+import base64
+from io import BytesIO
 
 
 # settle shipping
@@ -30,6 +33,7 @@ class SettleClearance(models.Model):
     total_amount = fields.Float(string='Total Amount', compute='get_total_amount', store=True)
     total_amount_invoice = fields.Float(string='Total Amount of Invoice', compute='get_total_amount', store=True)
     settle_clearance_detail_ids = fields.One2many('panexlogi.settle.clearance.detail', 'settle_clearance_id')
+    settle_clearance_output_ids = fields.One2many('panexlogi.settle.clearance.output', 'settle_clearance_id')
 
     @api.model
     def create(self, values):
@@ -130,6 +134,103 @@ class SettleClearance(models.Model):
                     }))
                 rec.settle_clearance_detail_ids = settle_clearance_detail
 
+    # print to excel
+
+    def print_detail_to_excel(self):
+        for rec in self:
+
+            if not rec.settle_clearance_detail_ids:
+                raise exceptions.ValidationError(_('No data to print!!'))
+
+            output = BytesIO()
+            workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+            worksheet = workbook.add_worksheet('Settle Clearance Details')
+
+            # Define the headers
+            headers = ['Invoice ID', 'Invoice No', 'Waybill ID', 'Job No', 'Waybill No', 'Container',
+                       'Container Quantity', 'Item', 'POA', 'T1', 'VAT defer notification', 'Import declaration',
+                       'Extra article', 'LFR', 'Amount',
+                       'Invoice Amount', 'Remark']
+
+            # Define a format with border
+            border_format = workbook.add_format({'border': 1})
+
+            for col_num, header in enumerate(headers):
+                worksheet.write(0, col_num, header, border_format)
+
+            # Write the data
+            row = 1
+            for detail in rec.settle_clearance_detail_ids:
+                worksheet.write(row, 0, detail.invoice_id.billno, border_format)
+                worksheet.write(row, 1, detail.invoiceno, border_format)
+                worksheet.write(row, 2, detail.waybill_id.billno, border_format)
+                worksheet.write(row, 3, detail.jobno, border_format)
+                worksheet.write(row, 4, detail.waybillno, border_format)
+                worksheet.write(row, 5, detail.Container, border_format)
+                worksheet.write(row, 6, detail.container_qty, border_format)
+                worksheet.write(row, 7, detail.fitem.name, border_format)
+                worksheet.write(row, 8, detail.poa, border_format)
+                worksheet.write(row, 9, detail.t1, border_format)
+                worksheet.write(row, 10, detail.vdn, border_format)
+                worksheet.write(row, 11, detail.imd, border_format)
+                worksheet.write(row, 12, detail.exa, border_format)
+                worksheet.write(row, 13, detail.lfr, border_format)
+                worksheet.write(row, 14, detail.amount, border_format)
+                worksheet.write(row, 15, detail.invoice_amount, border_format)
+                worksheet.write(row, 16, detail.remark, border_format)
+                row += 1
+
+            workbook.close()
+            output.seek(0)
+            # rec.pdffile = base64.b64encode(output.read())
+            # rec.pdffilename = 'Settle_Clearance_Details.xlsx'
+
+            # To send the generated Excel file as an attachment in a message
+            file_data = output.read()
+            rec.settle_clearance_output_ids.create({
+                'settle_clearance_id': rec.id,
+                'description': 'Settle Clearance Details(%s)' % fields.Datetime.now(),
+                'excel_file': base64.b64encode(file_data),
+                'excel_filename': 'Settle_Clearance_Details.xlsx'
+            })
+            output.close()
+
+            """
+            attachment = ('Settle_Clearance_Details.xlsx', file_data)
+            rec.message_post(
+                body='Please find the attached Settle Clearance Details.',
+                subject='Settle Clearance Details',
+                message_type='notification',
+                subtype_xmlid="mail.mt_comment",  # Correct subtype for emails
+                attachments=[attachment]
+            )
+            """
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Success',
+                'message': 'Output details successfully!',
+                'type': 'success',
+                'sticky': False,
+                'next': {
+                    'type': 'ir.actions.client',
+                    'tag': 'reload',
+                }
+            }
+        }
+
+    # check the combination of project,period is unique
+    @api.constrains('project', 'period')
+    def _check_project_period(self):
+        for rec in self:
+            domain = [('project', '=', rec.project.id)
+                , ('period', '=', rec.period.id)
+                , ('state', '!=', 'cancel')
+                , ('id', '!=', rec.id)]
+            if self.search_count(domain) > 0:
+                raise exceptions.ValidationError(_('The Project and Period must be unique!!'))
+
 
 class SettleClearanceDetail(models.Model):
     _name = 'panexlogi.settle.clearance.detail'
@@ -152,12 +253,32 @@ class SettleClearanceDetail(models.Model):
     exa = fields.Float(string='Extra article', tracking=True, default=0)
     lfr = fields.Float(string='LFR', tracking=True, default=0)
     amount = fields.Float(string='Amount（欧元金额）', compute='get_total_amount', store=True)
-    invoice_amount = fields.Float(string='Invoice Amount', tracking=True)
+    invoice_amount = fields.Float(string='Invoice Amount(发票金额)', tracking=True)
     remark = fields.Text(string='Remark', tracking=True)
 
     @api.depends('poa', 't1', 'vdn', 'imd', 'exa', 'lfr')
     def get_total_amount(self):
-        amount = 0
         for rec in self:
+            amount = 0
             amount += rec.poa + rec.t1 + rec.vdn + rec.imd + rec.exa + rec.lfr
             rec.amount = amount
+
+    @api.constrains('invoice_id')
+    def _check_invoice_id(self):
+        for rec in self:
+            if rec.invoice_id:
+                domain = [('invoice_id', '=', rec.invoice_id.id)
+                    , ('id', '!=', rec.id)
+                    , ('settle_clearance_id.state', '!=', 'cancel')]
+                if self.search_count(domain) > 0:
+                    raise exceptions.ValidationError(_('The Invoice ID must be unique!!'))
+
+
+class SettleClearanceOutput(models.Model):
+    _name = 'panexlogi.settle.clearance.output'
+    _description = 'panexlogi.settle.clearance.output'
+
+    settle_clearance_id = fields.Many2one('panexlogi.settle.clearance', string='Settle Clearance')
+    description = fields.Char(string='Description')
+    excel_file = fields.Binary(string='File')
+    excel_filename = fields.Char(string='File name')
--- mymodules/panexLogi/models/settle_shipping.py
+++ mymodules/panexLogi/models/settle_shipping.py
@@ -1,5 +1,8 @@
 from odoo import _, models, fields, api, exceptions
 from odoo.exceptions import UserError
+import xlsxwriter
+import base64
+from io import BytesIO
 
 
 # settle shipping
@@ -30,7 +33,7 @@ class SettleShipping(models.Model):
     total_amount = fields.Float(string='Total Amount', compute='get_total_amount', store=True)
     total_amount_usd = fields.Float(string='Total Amount USD', compute='get_total_amount', store=True)
     settle_shipping_detail_ids = fields.One2many('panexlogi.settle.shipping.detail', 'settle_shipping_id')
-
+    settle_shipping_output_ids = fields.One2many('panexlogi.settle.shipping.output', 'settle_clearance_id')
     @api.model
     def create(self, values):
         """
@@ -38,6 +41,7 @@ class SettleShipping(models.Model):
         """
         times = fields.Date.today()
         values['billno'] = self.env['ir.sequence'].next_by_code('seq.settle.shipping', times)
+        values['state'] = 'new'
         return super(SettleShipping, self).create(values)
 
     @api.model
@@ -80,6 +84,17 @@ class SettleShipping(models.Model):
             else:
                 rec.state = 'new'
                 return True
+
+    # check the combination of project,period is unique
+    @api.constrains('project', 'period')
+    def _check_project_period(self):
+        for rec in self:
+            domain = [('project', '=', rec.project.id)
+                , ('period', '=', rec.period.id)
+                , ('state', '!=', 'cancel')
+                , ('id', '!=', rec.id)]
+            if self.env['panexlogi.settle.shipping'].search_count(domain) > 0:
+                raise exceptions.ValidationError(_('The Project and Period must be unique!!'))
 
     # 计算总金额
     @api.depends('settle_shipping_detail_ids.amount', 'settle_shipping_detail_ids.amount_usd')
@@ -128,13 +143,110 @@ class SettleShipping(models.Model):
                         }))
                 rec.settle_shipping_detail_ids = settle_shipping_detail
 
+    # print to excel
+
+    def print_detail_to_excel(self):
+        for rec in self:
+
+            if not rec.settle_shipping_detail_ids:
+                raise exceptions.ValidationError(_('No data to print!!'))
+
+            output = BytesIO()
+            workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+            worksheet = workbook.add_worksheet('Settle Clearance Details')
+
+            # Define the headers
+            headers = ['Invoice ID', 'Invoice No', 'Waybill ID', 'Job No', 'Waybill No', 'Container',
+                       'Container Quantity', 'Item', 'Item_name', 'Amount_EUR', 'Amount_USD', 'Remark']
+
+            # Define a format with border
+            border_format = workbook.add_format({'border': 1})
+
+            for col_num, header in enumerate(headers):
+                worksheet.write(0, col_num, header, border_format)
+
+            # Write the data
+            row = 1
+            for detail in rec.settle_shipping_detail_ids:
+                worksheet.write(row, 0, detail.invoice_id.billno, border_format)
+                worksheet.write(row, 1, detail.invoiceno, border_format)
+                worksheet.write(row, 2, detail.waybill_id.billno, border_format)
+                worksheet.write(row, 3, detail.jobno, border_format)
+                worksheet.write(row, 4, detail.waybillno, border_format)
+                worksheet.write(row, 5, detail.Container, border_format)
+                worksheet.write(row, 6, detail.container_qty, border_format)
+                worksheet.write(row, 7, detail.fitem.code, border_format)
+                worksheet.write(row, 8, detail.fitem.name, border_format)
+                worksheet.write(row, 9, detail.amount, border_format)
+                worksheet.write(row, 10, detail.amount_usd, border_format)
+                worksheet.write(row, 11, detail.remark, border_format)
+                row += 1
+
+            workbook.close()
+            output.seek(0)
+            # rec.pdffile = base64.b64encode(output.read())
+            # rec.pdffilename = 'Settle_Clearance_Details.xlsx'
+
+            # To send the generated Excel file as an attachment in a message
+            file_data = output.read()
+            rec.settle_shipping_output_ids.create({
+                'settle_clearance_id': rec.id,
+                'description': 'Settle Shipping Details(%s)' % fields.Datetime.now(),
+                'excel_file': base64.b64encode(file_data),
+                'excel_filename': 'Settle_Shipping_Details.xlsx'
+            })
+            output.close()
+
+            """
+            attachment = ('Settle_Shipping_Details.xlsx', file_data)
+            rec.message_post(
+                body='Please find the attached Settle Clearance Details.',
+                subject='Settle Clearance Details',
+                message_type='notification',
+                subtype_xmlid="mail.mt_comment",  # Correct subtype for emails
+                attachments=[attachment]
+            )
+            """
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': 'Success',
+                'message': 'Output details successfully!',
+                'type': 'success',
+                'sticky': False,
+                'next': {
+                    'type': 'ir.actions.client',
+                    'tag': 'reload',
+                }
+            }
+        }
+
+    @api.constrains('project', 'period')
+    def _check_project_period(self):
+        for rec in self:
+            domain = [('project', '=', rec.project.id)
+                , ('period', '=', rec.period.id)
+                , ('state', '!=', 'cancel')
+                , ('id', '!=', rec.id)]
+            if self.search_count(domain) > 0:
+                raise exceptions.ValidationError(_('The Project and Period must be unique!!'))
+
 
 class SettleShippingDetail(models.Model):
     _name = 'panexlogi.settle.shipping.detail'
     _description = 'panexlogi.settle.shipping.detail'
 
+    # Add related project field
+    project_id = fields.Many2one(
+        'panexlogi.project',
+        string='Project',
+        related='settle_shipping_id.project',
+        store=True
+    )
     jobno = fields.Char(string='Job No')
     invoice_id = fields.Many2one('panexlogi.waybill.shipinvoice', string='Invoice ID')
+                                 #,domain=[('state', 'in', ['confirm', 'apply', 'paid']),('waybill_billno.project', '=', project_id.id)])
     invoiceno = fields.Char(string='Invoice No')
     waybill_id = fields.Many2one('panexlogi.waybill', string='Waybill ID')
     waybillno = fields.Char(string='BL No')
@@ -148,3 +260,33 @@ class SettleShippingDetail(models.Model):
     remark = fields.Text(string='Remark', tracking=True)
 
     settle_shipping_id = fields.Many2one('panexlogi.settle.shipping', string='Settle Shipping')
+
+    """
+    @api.onchange('invoice_id')
+    def _compute_waybill(self):
+        for rec in self:
+            rec.invoice_id = rec.invoice_id.invno
+            rec.waybill_id = rec.invoice_id.waybill_billno.id
+            rec.jobno = rec.invoice_id.waybill_billno.docno
+            rec.Container = ','.join([str(x) for x in rec.invoice_id.waybill_billno.details_ids.mapped('cntrno')])
+            rec.container_qty = len(rec.invoice_id.waybill_billno.details_ids.mapped('cntrno'))
+    
+    # check invoice_id unique
+    @api.constrains('invoice_id')
+    def _check_invoice_id(self):
+        for rec in self:
+            if rec.invoice_id:
+                domain = [('invoice_id', '=', rec.invoice_id.id)
+                    , ('id', '!=', rec.id)
+                    , ('settle_shipping_id.state', '!=', 'cancel')]
+                if self.search_count(domain) > 0:
+                    raise exceptions.ValidationError(_('The Invoice ID must be unique!!'))
+    """
+class SettleShippingOutput(models.Model):
+    _name = 'panexlogi.settle.shipping.output'
+    _description = 'panexlogi.settle.shipping.output'
+
+    settle_clearance_id = fields.Many2one('panexlogi.settle.shipping', string='Settle Shipping')
+    description = fields.Char(string='Description')
+    excel_file = fields.Binary(string='File')
+    excel_filename = fields.Char(string='File name')
